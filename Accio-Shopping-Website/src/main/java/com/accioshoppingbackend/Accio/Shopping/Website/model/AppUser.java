96481dd12291107362aff4fd51edbe2da516aa0c
keshav <keshavkumar584@gmail.com>
2025-05-02 13:31:01 +0530

    Resolved merge conflicts in pom.xml and application.properties

--- Accio-Shopping-Website/src/main/java/com/accioshoppingbackend/Accio/Shopping/Website/model/AppUser.java
+++ Accio-Shopping-Website/src/main/java/com/accioshoppingbackend/Accio/Shopping/Website/model/AppUser.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class AppUserr {
+public class AppUser {
     @Id
     private UUID id;
     private String name;
@@ -30,71 +30,4 @@ public class AppUserr {
     private String address;
     @Column(nullable = false)
     private String userType;
-
-@Entity
-public class AppUser {
-    @Id
-    int id;
-    String name;
-    @Column(unique = true, nullable = false)
-    String email;
-    Long phoneNumber;
-    String address;
-    String password;
-
-    public AppUser(String name, String email, Long phoneNumber, String address, String password) {
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
