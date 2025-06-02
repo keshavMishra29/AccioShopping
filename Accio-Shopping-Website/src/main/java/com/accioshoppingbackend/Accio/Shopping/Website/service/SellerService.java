aa7e96a3cdb09dc7f1f80b29f751c18face6c031
keshav <keshavkumar584@gmail.com>
2025-06-02 23:28:59 +0530

    feature/remove-product : Remove functionality added

--- Accio-Shopping-Website/src/main/java/com/accioshoppingbackend/Accio/Shopping/Website/service/SellerService.java
+++ Accio-Shopping-Website/src/main/java/com/accioshoppingbackend/Accio/Shopping/Website/service/SellerService.java
@@ -1,6 +1,7 @@
 package com.accioshoppingbackend.Accio.Shopping.Website.service;
 
 import com.accioshoppingbackend.Accio.Shopping.Website.exception.AccessNotFound;
+import com.accioshoppingbackend.Accio.Shopping.Website.exception.InvalidProductID;
 import com.accioshoppingbackend.Accio.Shopping.Website.exception.UserNotFound;
 import com.accioshoppingbackend.Accio.Shopping.Website.model.AppUser;
 import com.accioshoppingbackend.Accio.Shopping.Website.model.Product;
@@ -16,6 +17,8 @@ public class SellerService {
     ProductRepository productRepository;
     @Autowired
     private CommonUserService userService;
+    @Autowired
+    private ProductService productService;
     public void addProduct(Product product, UUID sellerID){
         AppUser user = userService.getUserById(sellerID);
         if (user == null){
@@ -26,5 +29,39 @@ public class SellerService {
         }
         product.setSeller(user);
         productRepository.save(product);
+    }
+    public String removeProduct(UUID sellerID, UUID productID){
+        Boolean isSeller = userService.isSeller(sellerID);
+        if (isSeller == null){
+            throw new UserNotFound(String.format("User with id %s does not exist", sellerID.toString()));
+        }
+        if (isSeller == false){
+            throw new AccessNotFound(String.format(
+                    "User with id %s does not have access to delete product",
+                    sellerID.toString()));
+        }
+        boolean validProduct = productService.ValidateProductID(productID);
+        if (validProduct == false){
+            throw new InvalidProductID(String.format(
+                    "Product with name %s does not exist in system",
+                    productID.toString()));
+        }
+        Product product = productService.getProductByID(productID);
+        System.out.println(product);
+        AppUser owner = product.getSeller();
+
+        if (!owner.getId().equals(sellerID)){
+            throw new AccessNotFound(String.format(
+                    "User with name %s does not have access to remove product %s",
+                    owner.getName(),
+                    product.getProductName()
+            ));
+        }
+
+        productService.removeProduct(product);
+        return String.format(
+                "Seller with name %s removed product %s",
+                owner.getName(),
+                product.getProductName());
     }
 }
